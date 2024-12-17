63d73b6f4a5c61ac947178221d3f3443940e95ae
Tudor-debug819 <tovidiua@emeal.nttdata.com>
2024-12-17 23:14:26 +0200

    working snowflakes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { SnowflakesComponent } from "./snowflakes/snowflakes.component";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [SnowflakesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/snowflakes/snowflakes.component.ts
+++ src/app/snowflakes/snowflakes.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-snowflakes',
@@ -7,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrl: './snowflakes.component.css'
 })
 export class SnowflakesComponent {
+  emoji = '❄';
+  particles = 300;
+  innerWidth = window.innerWidth;
+
+  constructor(private renderer: Renderer2, public el: ElementRef) { }
+
+  ngOnInit():void{
+    this.createParticles();
+  }
+
+  createParticles() {
+    for (let i = 0; i < this.particles; i++) {
+      let emojiLeftPosition = Math.random() * this.innerWidth; // Randomize left position
+      let span = this.renderer.createElement('span');
+      let text = this.renderer.createText(this.emoji);
+      this.renderer.appendChild(span, text);
+      this.renderer.addClass(span, 'snowflake');
+  
+      // Randomize position, duration, and delay
+      this.renderer.setStyle(span, 'left', emojiLeftPosition + 'px');
+      this.renderer.setStyle(span, 'animation-duration', this.randomMinMax(5, 10) + 's');
+      this.renderer.setStyle(span, 'animation-delay', this.randomMinMax(0, 10) + 's');
+  
+      this.renderer.appendChild(this.el.nativeElement, span);
+    }
+  }
+
+  randomMinMax(min: number, max: number) {
+    return Math.random() * (max - min) + min;
+  }
 
 }
