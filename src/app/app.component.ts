1738c6a9d7f873fcefedd1d6842f2f146048a91d
Tudor-debug819 <tovidiua@emeal.nttdata.com>
2024-12-17 23:19:37 +0200

    adding background image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { SnowflakesComponent } from "./snowflakes/snowflakes.component";
 
 @Component({
