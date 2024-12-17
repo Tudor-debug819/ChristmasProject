7bb3e9332f632490d3616ca872fc4202035f663b
Tudor-debug819 <tovidiua@emeal.nttdata.com>
2024-12-17 22:29:09 +0200

    make new component css

---
+++ src/app/snowflakes/snowflakes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-snowflakes',
+  imports: [],
+  templateUrl: './snowflakes.component.html',
+  styleUrl: './snowflakes.component.css'
+})
+export class SnowflakesComponent {
+
+}
